950ce98ca87828bfb01a9ec10fc4c07720de6e89
tomko3ch@gmail.com <tomko3ch@gmail.com>
2020-08-05 09:22:07 +0300

    added routes to map the app

--- wallpapers/urls.py
+++ wallpapers/urls.py
@@ -1,6 +1,13 @@
 from django.urls import path
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
-    path('', views.home, name='wallpapers.home'),
+    path('', views.home, name='wallpapers-home'),
+    path('search/', views.search_results, name='wallpapers-search'),
+    path('image/<int:id>', views.image_by_id, name='wallpapers-single_image'),
+    path('location/<int:id>', views.images_by_location, name='wallpapers-image_location'),
 ]
+if settings.DEBUG:
+    urlpatterns+= static(settings.MEDIA_URL,document_root = settings.MEDIA_ROOT)
