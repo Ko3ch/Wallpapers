2c2f62abd435319f9a04d00c0c1edfd7c6e3a92e
tomko3ch@gmail.com <tomko3ch@gmail.com>
2020-08-05 10:01:34 +0300

    added heroku deployment configurations

--- wallpaper_gallery/settings.py
+++ wallpaper_gallery/settings.py
@@ -49,6 +49,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'wallpaper_gallery.urls'
@@ -129,3 +130,5 @@ STATICFILES_DIRS = [
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+django_heroku.settings(locals())
